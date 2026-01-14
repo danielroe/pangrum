890f0abd5aecab0b64f0f5102754901fdbf68ae4
Daniel Roe <daniel@roe.dev>
2026-01-14 21:39:09 +0000

    fix: try custom preset

--- pwa-assets.config.ts
+++ pwa-assets.config.ts
@@ -1,11 +1,29 @@
-import { combinePresetAndAppleSplashScreens, defineConfig, minimal2023Preset } from '@vite-pwa/assets-generator/config'
+import type { Preset } from '@vite-pwa/assets-generator/config'
+import { combinePresetAndAppleSplashScreens, defineConfig } from '@vite-pwa/assets-generator/config'
+
+const preset: Preset = {
+  transparent: {
+    sizes: [64, 192, 512],
+    favicons: [[48, 'favicon.ico']],
+    padding: 0,
+  },
+  maskable: {
+    sizes: [512],
+    padding: 0,
+  },
+  apple: {
+    sizes: [180],
+    // logo.svg already has proper spacing
+    padding: 0,
+  },
+}
 
 export default defineConfig({
   headLinkOptions: {
     preset: '2023',
   },
   preset: combinePresetAndAppleSplashScreens(
-    minimal2023Preset,
+    preset,
     {
       padding: 0.3,
       resizeOptions: { background: '#fafaf9', fit: 'contain' },
