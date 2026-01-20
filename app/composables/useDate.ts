87fa85e0a258e272ae7f2ad722abf8bcddfabda4
Daniel Roe <daniel@roe.dev>
2026-01-20 19:31:21 +0000

    fix: simplify

--- app/composables/useDate.ts
+++ app/composables/useDate.ts
@@ -1,17 +1,14 @@
 function isValidDate(dateString: unknown): dateString is string {
-  if (typeof dateString !== 'string') return false
-  const regex = /^\d{4}-\d{2}-\d{2}$/
-  if (!regex.test(dateString)) return false
-  const date = new Date(dateString)
-  const timestamp = date.getTime()
-  if (isNaN(timestamp)) return false
-  return date.toISOString().slice(0, 10) === dateString
+  return typeof dateString === 'string' && new Date(dateString).toISOString().slice(0, 10) === dateString
 }
 
 export const useDate = createSharedComposable(function useDate() {
   const route = useRoute()
   const router = useRouter()
-  const dateString = isValidDate(route.query.date) ? route.query.date : new Date().toISOString().slice(0, 10)
+
+  const dateString = isValidDate(route.query.date)
+    ? route.query.date
+    : new Date().toISOString().slice(0, 10)
 
   const date = ref(import.meta.server ? '' : dateString)
   watch(date, newDate => router.push({ query: { ...route.query, date: newDate } }), { immediate: true })
